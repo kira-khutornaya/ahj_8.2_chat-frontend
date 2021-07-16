371b641c775c62c22f95070d8489f1a6ecd0fbca
Kira Khutornaya <kira.khutornaya@mail.ru>
2021-07-16 11:27:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat (frontend)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/pharvr72285tp2rf?svg=true)](https://ci.appveyor.com/project/kira-khutornaya/ahj-8-2-chat-frontend)
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import ChatManager from './ChatManager';
+
+const chatManager = new ChatManager('.chat');
+chatManager.bindToDOM();
+
+// const chat = document.querySelector('.chat');
+// console.log(chat)
+// const login = new Login(chat)
+// login.showPopup();
